d3d47295fe1d89d25ec40cc511933bfc80a31d4b
ashtn15kimsey <ashtn15@gmail.com>
2021-03-14 19:45:28 -0700

    update

---
+++ JavaScript/JavaScript_3.js
@@ -0,0 +1,6 @@
+function displayType(character) {
+    var charcterType=character.getAttributes("data-character-type");
+    alert(charcterType + "is int the" + character.innerHTML+ "accomplishment!");
+}
+
+
